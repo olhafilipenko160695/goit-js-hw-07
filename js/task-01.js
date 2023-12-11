ba60ea456e9830b17ed92eb25c3741ec38b45bff
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-11 15:21:05 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,8 @@
 const allCategories = document.querySelectorAll(".item");
 console.log("Number of categories:", allCategories.length);
+
+const newArray = [...allCategories];
+newArray.forEach(element => {
+    console.log("Category:", element.firstElementChild.textContent);
+    console.log("Elements:", element.lastElementChild.children.length);
+});

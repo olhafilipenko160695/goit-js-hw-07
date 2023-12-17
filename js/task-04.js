6e14c70321867fef69b02bd0c0cf51bcf8c763c5
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-17 23:53:58 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,4 @@
 const loginForm = document.querySelector('.login-form');
-const loginEmail = document.querySelector('[name="email"]');
-const loginPassword = document.querySelector('[name="password"]');
-
 const button = document.querySelector('[type ="submit"]');
 
 loginForm.addEventListener('submit', sendForm);
@@ -11,21 +8,21 @@ function sendForm(event) {
  
     let formElements = this.elements;
     let formData = {};
-    // console.log(formElements);
-    
 
+    
     for (let i = 0; i < formElements.length; i += 1){
         let element = formElements[i];
-// console.dir(element);
+        
         if (element.tagName === "INPUT" && element.type !== 'submit') {
-            formData[element.name] = element.value.trim();
-        }
-    }
-       if (loginEmail.value === '' || loginPassword.value === "") {
+             if (loginForm.elements.email.value === '' || loginForm.elements.password.value === '') {
         alert('All form fields must be filled in')
+             } else {
+            formData[element.name] = element.value.trim();
        }
-    
+      }
+    }
     console.log(formData);
 
     this.reset();
 }
+

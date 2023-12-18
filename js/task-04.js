458be544a812b850d804b92a8b64153049cb7c61
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-18 23:15:37 +0100

    1

--- js/task-04.js
+++ js/task-04.js
@@ -1,28 +1,30 @@
-const loginForm = document.querySelector('.login-form');
+const loginForm = document.querySelector(".login-form");
 const button = document.querySelector('[type ="submit"]');
 
-loginForm.addEventListener('submit', sendForm);
+loginForm.addEventListener("submit", sendForm);
 
 function sendForm(event) {
-    event.preventDefault();
- 
-    let formElements = this.elements;
-    let formData = {};
+  event.preventDefault();
 
-    
-    for (let i = 0; i < formElements.length; i += 1){
-        let element = formElements[i];
-        
-        if (element.tagName === "INPUT" && element.type !== 'submit') {
-             if (loginForm.elements.email.value === '' || loginForm.elements.password.value === '') {
-        alert('All form fields must be filled in')
-             } else {
-            formData[element.name] = element.value.trim();
-       }
-      }
+  let formElements = this.elements;
+  let formData = {};
+
+  for (let i = 0; i < formElements.length; i += 1) {
+    let element = formElements[i];
+
+    if (element.tagName === "INPUT" && element.type !== "submit") {
+      formData[element.name] = element.value.trim();
     }
+  }
+
+  // Check if email or password is empty after the loop
+  if (
+    loginForm.elements.email.value === "" ||
+    loginForm.elements.password.value === ""
+  ) {
+    alert("All form fields must be filled in");
+  } else {
     console.log(formData);
-
     this.reset();
+  }
 }
-

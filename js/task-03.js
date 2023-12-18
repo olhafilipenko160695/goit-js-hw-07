c9bec4f882865ad9cac2213292a622a4adf73693
olhafilipenko160695 <olyaao95@gmail.com>
2023-12-18 22:05:45 +0100

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -5,6 +5,6 @@ const inputCurrentValue = document.querySelector('#name-output');
 
 
 function handlerInput(event) {
-    event.currentTarget.value.includes(null) || event.currentTarget.value.trim() === '' ? inputCurrentValue.textContent = "Anonymous" : inputCurrentValue.textContent = event.currentTarget.value.trim();
+ inputCurrentValue.textContent = event.currentTarget.value.trim() || 'Anonymous';
 }
 
